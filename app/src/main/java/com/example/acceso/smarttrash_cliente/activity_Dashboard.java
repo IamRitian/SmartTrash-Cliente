ce80b4b22925e55ce8f5477fd326b3e67c3a016d
Merzee08 <merce.villegas@hotmail.com>
2018-11-04 18:08:05 -0700

    Recuperé el activity_Dashboard

--- app/src/main/java/com/example/acceso/smarttrash_cliente/activity_Dashboard.java
+++ app/src/main/java/com/example/acceso/smarttrash_cliente/activity_Dashboard.java
@@ -11,14 +11,19 @@ import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.LegendEntry;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.DataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.PercentFormatter;
 
 import java.util.ArrayList;
 
-public class Dashboard extends AppCompatActivity {
-
+public class activity_Dashboard extends AppCompatActivity {
     /**
      * MERCE
      */
@@ -28,17 +33,17 @@ public class Dashboard extends AppCompatActivity {
     private int[]points= new int[]{25, 30, 38, 15, 28, 20, 10, 16, 34, 40, 23, 12};
     private int[]colors= new int[]{Color.RED, Color.YELLOW, Color.GREEN, Color.CYAN, Color.BLUE, Color.MAGENTA, Color.RED, Color.YELLOW, Color.GREEN, Color.CYAN, Color.BLUE, Color.MAGENTA};
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
-        /**
-         * MERCE
-         */
+        setContentView(R.layout.activity__dashboard);
+
         barChart=(BarChart)findViewById(R.id.barChart);
         pieChart=(PieChart)findViewById(R.id.pieChart);
+
+        
     }
+
     /**
      * MERCEE
      */
@@ -56,7 +61,7 @@ public class Dashboard extends AppCompatActivity {
         legend.setForm(Legend.LegendForm.CIRCLE);
         legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
 
-        ArrayList<LegendEntry> entries = new ArrayList<>();
+        ArrayList<LegendEntry> entries=new ArrayList<>();
         for(int i=0;i<months.length;i++){
             LegendEntry entry=new LegendEntry();
             entry.formColor=colors[i];
@@ -99,10 +104,43 @@ public class Dashboard extends AppCompatActivity {
         barChart=(BarChart)getSameChart(barChart, "Series", Color.BLACK, Color.WHITE, 2000);
         barChart.setDrawGridBackground(true);
         barChart.setDrawBarShadow(true);
+        barChart.setData(getBarData());
+        barChart.invalidate();
 
         axisX(barChart.getXAxis());
         axisLeft(barChart.getAxisLeft());
         axisRight(barChart.getAxisRight());
+
+        pieChart=(PieChart)getSameChart(pieChart, "Puntos", Color.GRAY, Color.WHITE, 2000);
+        pieChart.setHoleRadius(10);
+        pieChart.setTransparentCircleRadius(12);
+        pieChart.setDrawHoleEnabled(false);
+        pieChart.setData(getPieData());
+        pieChart.invalidate();
     }
 
+    private DataSet getData(DataSet dataSet){
+    dataSet.setColors(colors);
+    dataSet.setValueTextColor(Color.WHITE);
+    dataSet.setValueTextSize(10);
+    return dataSet;
+    }
+
+    private BarData getBarData(){
+        BarDataSet barDataSet=(BarDataSet)getData(new BarDataSet(getBarEntries(),""));
+
+        barDataSet.setBarShadowColor(Color.GRAY);
+        BarData barData=new BarData(barDataSet);
+        barData.setBarWidth(0.45f);
+        return barData;
+    }
+
+    private PieData getPieData(){
+        PieDataSet pieDataSet=(PieDataSet)getData(new PieDataSet(getPieEntries(),""));
+
+        pieDataSet.setSliceSpace(1);
+        pieDataSet.setValueFormatter(new PercentFormatter());
+
+        return new PieData(pieDataSet);
+    }
 }
